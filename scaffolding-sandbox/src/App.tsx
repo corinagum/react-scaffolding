c9f6e26c4fe0d53df4d842b2ea2a006485e7bc2a
Corina Gum <>
2022-10-20 22:16:46 -0700

    First commit for sandbox

---
+++ scaffolding-sandbox/src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>Nothing here yet</p>
+        <p>
+          <a href='https://github.com/corinagum/react-scaffolding'>Check out the react-scaffolding GitHub repo</a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
